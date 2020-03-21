49c2a9d058788e67a450d04909997d3749ed0cd5
Brad Phillips <bjrp@protonmail.com>
2020-03-21 22:25:19 +0000

    Password length is validated

--- lib/password.rb
+++ lib/password.rb
@@ -11,4 +11,8 @@ class Password
   def set_password(password)
     @password = password
   end
+
+  def length_validator
+    raise 'Password must be between 8 and 18 characters' if @password.length <= 7 || @password.length >=19
+  end
 end
--- spec/password_spec.rb
+++ spec/password_spec.rb
@@ -7,11 +7,20 @@ describe Password do
   it 'initializes as an empty string' do
     expect(subject.access_password).to eq('')
   end
-
   describe '#set_password' do
     it 'sets the password' do
       subject.set_password('Testing1.')
       expect(subject.access_password).to eq('Testing1.')
+    end
+  end
+  describe '#length_validator' do
+    it 'checks the length is at least 8 characters' do
+      subject.set_password('Testi1.')
+      expect { subject.length_validator }.to raise_error(RuntimeError, 'Password must be between 8 and 18 characters')
+    end
+    it 'checks the length is at most 18 characters' do
+      subject.set_password('Testingtestingtesting1.')
+      expect { subject.length_validator }.to raise_error(RuntimeError, 'Password must be between 8 and 18 characters')
     end
   end
 end
